c2ece8df983f2f992479c7aa0032d4a5d436c5f9
Mussadiq32 <120275986+Mussadiq32@users.noreply.github.com>
2025-03-08 11:31:07 +0530

    Update theme-toggle.tsx

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/Button"
 import { cn } from "@/lib/utils"
 
 export function ThemeToggle({ className }: { className?: string }) {
